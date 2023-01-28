b5367cb9c0117793b149ccad29e393d3eed7a18f
“Irina <irina.osipova2012@gmail.com>
2023-01-28 20:04:33 +0200

    feat: first test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 
 // You should implement your task here.
 
+function towelSort (matrix) {
+  
+  for (i=0; i<matrix.length && matrix[i].length>1; i++) {
+    if (i%2 !== 0) {
+      matrix[i].reverse();
+    } 
+  }
+  return matrix.join().split(',').map(Number);
+  
+  }
+
+
+/*
+towelSort function should expect matrix of any shape for example:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ],
+]
+Following matrix should be 'sorted' to:
+
+ [1, 2, 3, 6, 5, 4, 7, 8, 9 ]
+
+*/
+
+
+
 module.exports = function towelSort (matrix) {
   return [];
 }
